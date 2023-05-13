e012a78ed7e7d0eff20f573b5e5eff400b9b276b
channaveer <channaveerpatil999@gmail.com>
2023-05-13 18:24:32 +0530

    env file created

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import morgan from 'morgan';
 import userRoute from './routes/user.js';
+import dotenv from 'dotenv';
 
 const app = express();
 
@@ -11,10 +12,8 @@ app.use(express.json({ limit: '30mb', extended: true }));
 app.use(express.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
-const MONGODB =
-  'mongodb+srv://mern:mern1234@cluster0.uoqewvt.mongodb.net/mern?retryWrites=true&w=majority';
-
 const port = 5000;
+dotenv.config();
 
 app.get('/', (req, res) => {
   res.send('hello express');
@@ -24,10 +23,10 @@ app.use(express.json());
 app.use('/users', userRoute);
 
 mongoose
-  .connect(MONGODB)
+  .connect(process.env.MONGODB)
   .then(() => {
     app.listen(port, () => {
-      console.log(`listening on port${port}`);
+      console.log(`listening on port ${port}`);
     });
   })
   .catch((err) => {
